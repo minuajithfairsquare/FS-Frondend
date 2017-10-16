84fa49b5d7e7f41713c81302116ec9bae666e9f6
minuajith <minuajith@ip-10-37-221-43.eu-west-2.compute.internal>
2017-10-16 14:16:15 +0100

    test

--- scripts/setpermission.sh
+++ scripts/setpermission.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Set ownership for all folders
 chown -R www-data:www-data /var/www/html/uat
-
+chown -R www-data:www-data /var/www/html/uat
 # set files to 644 [except *.pl *.cgi *.sh]
-find /var/www/html/uat -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
+find /var/www/html/ -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
 # set folders to 755
-find /var/www/html/uat -type d -print0 | xargs -0 chmod 0755
+find /var/www/html/ -type d -print0 | xargs -0 chmod 0755
