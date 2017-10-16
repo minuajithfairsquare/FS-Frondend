607ff44a4e7f4e23d207a8b8fefc92c3b5371395
minuajithfairsquare <32395019+minuajithfairsquare@users.noreply.github.com>
2017-10-16 14:18:08 +0100

    Update setpermission.sh

--- scripts/setpermission.sh
+++ scripts/setpermission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set ownership for all folders
 chown -R www-data:www-data /var/www/html/uat
-chown -R www-data:www-data /var/www/html/uat
+chown -R www-data:www-data /var/www/html/fronapiuat
 # set files to 644 [except *.pl *.cgi *.sh]
 find /var/www/html/ -type f -not -name ".pl" -not -name ".cgi" -not -name "*.sh" -print0 | xargs -0 chmod 0644
 
